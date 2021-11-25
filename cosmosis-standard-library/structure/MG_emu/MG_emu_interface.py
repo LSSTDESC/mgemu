62bad9332b30da2a34a3998cf1b8bcf082da6c55
gqcristhian <gqcristhian@utdallas.edu>
2021-11-24 18:46:45 -0600

    adding module to run MG emulator using cosmosis... Testing with 3x2pt still missing but output seems fine

---
+++ cosmosis-standard-library/structure/MG_emu/MG_emu_interface.py
@@ -0,0 +1,101 @@
+# import regular cosmosis packages
+from builtins import str
+import os
+from cosmosis.datablock import names, option_section
+import sys
+import traceback
+# import packages for g(R) emulator
+from mgemu import emu
+import pyccl as ccl
+import numpy as np
+
+# These are pre-defined strings we use as datablock
+# section names
+modified_gravity_parameters = names.modified_gravity_parameters
+cosmo = names.cosmological_parameters
+distances = names.distances
+matter_power_nl = names.matter_power_nl
+
+# Read options from the ini file which are called once per processor
+def setup(options):
+    config = {
+        'zmin': options.get_double(option_section, 'zmin', default=0.0),
+        'zmax': options.get_double(option_section, 'zmax', default=1.0),
+        'num_z': options.get_int(option_section, 'num_z', default=150),
+        'do_distances': options.get_bool(option_section, 'do_distances', default = True),
+        'transfer_function': options.get_string(option_section, 'transfer_function', default='boltzmann_class'),
+    }
+    return config
+
+# Execute module is called every time for each set of cosmological parameters
+def execute(block, config):
+    # Get array of redshift to calculate P_MG(k)
+    z = np.linspace(config['zmin'], config["zmax"], config['num_z'])
+
+    # Call mgemu and ccl
+    pk_mg, k, chi = get_observable(Om  = block[cosmo, "omega_m"],
+                   h   = block[cosmo, "h0"],
+                   s8  = block[cosmo, "sigma_8"],
+                   ns  = block[cosmo, "n_s"],
+                   fR0 = block[modified_gravity_parameters, "fR0"],
+                   n   = block[modified_gravity_parameters, "n"],
+                   z   = z,
+                   Omb  = block[cosmo, "omega_b"]
+    )
+
+    # Saving results in a grid of (k,z,pk). Here k and pk already in h/Mpc and (Mpc/h)^3 units
+    print(pk_mg, k, z)
+    print(np.shape(pk_mg), np.shape(k), np.shape(z))
+    block.put_grid("matter_power_nl", "k_h", k, "z", z, "p_k", pk_mg.T)
+
+    # Pass chi to block[distances, 'd_m'], to use later in /structure/project_2d.py file.
+    block[distances, 'd_m'] = chi
+
+    return 0
+
+# Function to get pk_mg (not the ratio) and k arrays
+def get_observable(Om, h, s8, ns, fR0, n, z, Omb=0.02203):
+
+    # Call MG emulator
+    Omh2 = (h**2)*Om
+    pkratio = []
+    k = []
+    for i in range(0,len(z)):
+        print(Omh2, ns, s8, fR0, n, z[i])
+        pkratio_tmp, k_tmp = emu(Omh2, ns, s8, fR0, n, z[i])
+        print(pkratio_tmp, k_tmp)
+        pkratio.append(pkratio_tmp)
+        k.append(k_tmp)
+
+    # Convert list to numpy arrays
+    pkratio = np.array(pkratio)
+    k = np.array(k)
+
+    # Define CCL cosmology object
+    cosmo = ccl.Cosmology(Omega_c = Om - Omb / (h**2),
+                            Omega_b = Omb / (h**2),
+                            h = h,
+                            sigma8 = s8,
+                            n_s = ns,
+                            Neff = 3.046,
+                            transfer_function = config['transfer_function'],
+                            matter_power_spectrum = 'emu')
+    # Get P_MG(k)
+    a = 1./(1+z)
+    #Now we have to be careful, because mgemu k-units are in h/Mpc, while CCL units are in 1/Mpc. convert
+    kccl = k*h
+    pk_nl = ccl.nonlin_matter_power(cosmo, kccl, a)
+    #CCL output pk is in Mpc^3, convert to (Mpc/h)^3
+    pk_nl *= h*h*h
+    pk_mg = pk_nl*pkratio
+    print(pk_mg)
+
+    # Get radial comoving distance
+    if config['do_distances']==True:
+        chi = ccl.comoving_radial_distance(cosmo, 1/(1+z))
+    print(chi)
+    return pk_mg, k, chi
+
+# Not needed in Python (purpose is to free memory)
+def cleanup(config):
+    pass
